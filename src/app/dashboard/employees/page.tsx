9a1dcef08849e1ec4dad0df7df2b97f8d9061d8c
Adel Cherif <wlidha09@gmail.com>
2025-09-13 13:46:15 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/dashboard/employees/page.tsx
+++ src/app/dashboard/employees/page.tsx
@@ -107,10 +107,6 @@ export default function EmployeesPage() {
     }
   }
   
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="flex flex-col gap-6">
       <div className="flex items-center justify-between">
@@ -129,10 +125,14 @@ export default function EmployeesPage() {
           </Button>
         )}
       </div>
-      <EmployeeTable 
-        data={employees} 
-        onEditEmployee={handleEditEmployee} 
-      />
+       {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <EmployeeTable 
+          data={employees} 
+          onEditEmployee={handleEditEmployee} 
+        />
+      )}
 
       {isFormOpen && (
         <EmployeeForm
